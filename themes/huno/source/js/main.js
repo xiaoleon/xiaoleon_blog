8475793b0bb2e09f97ee87df3248011cd53fb57b
zhangxiao <xiao_leon@qq.com>
2018-01-24 22:44:54 +0800

    修改样式

--- themes/huno/source/js/main.js
+++ themes/huno/source/js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('a.blog-button').click(function() {
+  $('a.blog-button').click(function () {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     currentWidth = $('.panel-cover').width();
     $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
@@ -11,16 +11,16 @@ $(document).ready(function() {
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
-  if (window.location.pathname != "/") {       // if hexo in subdir of site, should change this line
+  if (window.location.pathname != "/") { // if hexo in subdir of site, should change this line
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
-  $('.btn-mobile-menu').click(function() {
+  $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
 
-  $('.navigation-wrapper .blog-button').click(function() {
+  $('.navigation-wrapper .blog-button').click(function () {
     // $('.navigation-wrapper').toggleClass('visible');
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
